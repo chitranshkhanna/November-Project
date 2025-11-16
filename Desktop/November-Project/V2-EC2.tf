aca8eb842341e739ddb2eca9fa7a0b200192ca4d
Chitransh Khanna <chitranshkhannaa26@gmail.com>
2025-11-16 14:14:07 +0530

    first commit

--- Desktop/November-Project/V2-EC2.tf
+++ Desktop/November-Project/V2-EC2.tf
@@ -2,34 +2,92 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-resource "aws_instance" "demo-server" {
-  ami = "ami-0157af9aea2eef346"
-  instance_type = "t2.micro"
-  key_name = "dpp"
-  security_groups = [ "demo-sg" ]
+resource "aws_vpc" "dpp_vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "dpp-vpc"
+  }
 }
 
-resource "aws_security_group" "demo-sg" {
-  name = "demo-sg"
+resource "aws_subnet" "dpp_public_subnet_01" {
+  vpc_id                  = aws_vpc.dpp_vpc.id
+  cidr_block              = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dpp-public-subnet-01"
+  }
+}
+
+resource "aws_subnet" "dpp_public_subnet_02" {
+  vpc_id                  = aws_vpc.dpp_vpc.id
+  cidr_block              = "10.1.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dpp-public-subnet-02"
+  }
+}
+
+resource "aws_internet_gateway" "dpp_igw" {
+  vpc_id = aws_vpc.dpp_vpc.id
+  tags = {
+    Name = "dpp-igw"
+  }
+}
+
+resource "aws_route_table" "dpp_public_rt" {
+  vpc_id = aws_vpc.dpp_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dpp_igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta_public_01" {
+  subnet_id      = aws_subnet.dpp_public_subnet_01.id
+  route_table_id = aws_route_table.dpp_public_rt.id
+}
+
+resource "aws_route_table_association" "rta_public_02" {
+  subnet_id      = aws_subnet.dpp_public_subnet_02.id
+  route_table_id = aws_route_table.dpp_public_rt.id
+}
+
+resource "aws_security_group" "demo_sg" {
+  name        = "demo-sg"
   description = "SSH Access"
+  vpc_id      = aws_vpc.dpp_vpc.id
 
   ingress {
     description = "ssh access"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
   tags = {
     Name = "ssh-port"
   }
+}
+
+resource "aws_instance" "demo_server" {
+  ami                    = "ami-0ecb62995f68bb549"
+  instance_type          = "t2.micro"
+  key_name               = "dpp"
+  subnet_id              = aws_subnet.dpp_public_subnet_01.id
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+
 }
